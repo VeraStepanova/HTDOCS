73bbe36fadf6fc6accd42411350e9afd32798129
Vera Stepanova <stepa.vera@gmail.com>
2021-01-27 13:24:55 +0100

    upd

--- uppgift2/index.php
+++ uppgift2/index.php
@@ -37,52 +37,37 @@ include_once 'api/App.php';
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Topplistan Data & IT</h2>
                     <h3 class="section-subheading text-muted"></h3>
-                </div>
-                
-            
-                    
+                </div>                    
                        <?php
                        App::main();
-                       ?>
-
-                                        
-                
+                       ?>               
             </div>
         </section> 
           <!-- Team-->
+
           <section class="page-section bg-light" id="team">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading text-uppercase">Fråga oss</h2>
                 </div>
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-7">
                         <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" />
-                            <h4>Kay Garland</h4>
-                            <p class="text-muted">Lead Designer</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                            <img class="mx-auto rounded-circle" src="assets/img/team/pic_Vera.jpg" alt="" />
+                            <h4>Vera Stepanova</h4>
+                            <p class="text-muted">Vera.Stepanova@yh.nackademin.se</p>
                         </div>
                     </div>
                     <div class="col-lg-4">
                         <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
-                            <h4>Larry Parker</h4>
-                            <p class="text-muted">Lead Marketer</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                            <img class="mx-auto rounded-circle" src="assets/img/team/Denitsa.jpg" alt="" />
+                            <h4>Denitsa Dencheva</h4>
+                            <p class="text-muted">Denitsa.Dencheva@yh.nackademin.se
+</p>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-                </div>
-            </div>
-        </section>
+         
+        
         
     </body>
 </html>
