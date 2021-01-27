9e8b21082012483b697ccc47369ee2b0e5f108bd
Vera Stepanova <stepa.vera@gmail.com>
2021-01-27 19:24:43 +0100

    upd

--- uppgift3/api/App.php
+++ uppgift3/api/App.php
@@ -33,15 +33,21 @@ class App
             $table .= "
         
             <div class= row align-items-center no-gutters mb-4 mb-lg-5>
-                    <div class= 'col-xl-3 col-lg-4'><img class='img-fluid mb-3 mb-lg-0' src=$value[image]></div>         
-                    <div class='col-xl-9 col-lg-8'> 
-                    <h4>$value[title]</h4>                   
+                    <div class= 'col-xl-3 col-lg-4'>    
+                    <img - fluid' src='/Webshop2/img/$value[image]'></div>         
+                    
+                    <div 
+                    class='col-xl-9 col-lg-8'> 
+                    <h4>$value[title]</h4>                                      
                     <div class='timeline-body'><p class='text-muted'>$value[description]</p></div>        
                     <h5 class='subheading'> Pris: $value[price]</h5>
                     <h5 class='subheading'>Antal i lager:  $value[antal]</h5>
+                    <button class='btn btn-primary btn-xl text-uppercase' id='sendMessageButton' type='submit>Send Message</button>
+                    <button type='button'>Köp</button>
                     </div>
-            </div>
-                    <hr>";
+                   
+            </div> <hr>
+            ";
         }
         echo $table;
     }
---
+++ uppgift3/index.php
@@ -0,0 +1,132 @@
+<?php
+
+include_once 'api/App.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Webshop</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+     <!-- Navigation-->
+     <nav class="navbar navbar-expand-lg navbar-dark" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets\img\logos\DATA_IT.PNG" alt="" /></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ml-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#about">Böcker</a></li>
+                        <li class="nav-item"><a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#team">Kundvagn</a></li>
+                        <li class="nav-item"><a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#contact">Kontakta oss</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Välkommen Till Vår Bokhandel!</div>
+                <div class="masthead-heading text-uppercase">duktiga programmerare</div>
+            </div>
+        </header>     
+        <!-- About-->
+        <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Topplistan Data & IT</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>                    
+                       <?php
+                       App::main();
+                       ?>               
+            </div>
+        </section> 
+          
+          
+          
+           <!-- Kundvagn-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Din kundvagn är tom</h2>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+                
+                        </div>
+                    </div>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Kontakta oss</h2>
+                </div>
+                <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" id="name" type="text" placeholder="Ditt Namn *" required="required" data-validation-required-message="Please enter your name." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" id="email" type="email" placeholder="Din E-post *" required="required" data-validation-required-message="Please enter your email address." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <input class="form-control" id="phone" type="tel" placeholder="Din Telefon *" required="required" data-validation-required-message="Please enter your phone number." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group form-group-textarea mb-md-0">
+                                <textarea class="form-control" id="message" placeholder="Ditt Meddelande *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <div id="success"></div>
+                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Skicka</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-8 text-lg-left">Copyright © Denitsa Dencheva & Vera Stepanova 2021</div>
+                    <div class="col-lg-4 text-lg-right">
+                        <a class="mr-3" href="#!">Privacy Policy</a>
+                        <a href="#!">Terms of Use</a>
+                    </div>
+                </div>
+            </div>
+    </body>
+    
+</html>
